fbcb2644d192ee1172a8fea3ac3b411f9d08c3af
Sky Readick <szreadick@gmail.com>
2017-10-22 22:23:56 -0400

    Update Landing Page

--- imports/ui/components/PublicHeader.js
+++ imports/ui/components/PublicHeader.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const PublicHeader = (props) => (
-  <div className="nav-header">
+  <div className="nav-header public">
     <div className="nav-header__content">
-      <Link className="nav-header__link--title" to="/">{props.title}</Link>
-      <Link className="nav-header__link" to="/login">Login</Link>
+      <Link className="nav-header__link--title public" to="/">{props.title}</Link>
+      <Link className="nav-header__link public" to="/login">Login</Link>
     </div>
   </div>
 );
--- imports/ui/pages/Landing.js
+++ imports/ui/pages/Landing.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
+// import { HashLink as Link } from 'react-router-hash-link';
+import { Link } from 'react-router-dom';
+import ScrollableAnchor, {configureAnchors} from 'react-scrollable-anchor'
 import { Accounts } from 'meteor/accounts-base';
 import PropTypes from 'prop-types';
 
+configureAnchors({scrollDuration: 800});
 export default Landing = (props) => (
   <div>
     <div className="landing__section">
-      <div className="flex-wrapper">
-        <h1 className="main_heading">Lyme Disease Management, Made <span>Easy</span>.</h1>
-        <p>LymeLog is your personal tool for simplifying your treatment process while faciliating recovery, all in a convenient and user-friendly interface.</p>
-        <Link className="landing__button" to="/Signup">Sign up now</Link>
-        {/* <Link className="down_icon" to="#2"><i className="huge angle double down icon"></i></Link> */}
-      </div>
+      <div className="landing__section__flex-wrapper">
+        <h1 className="landing__main-heading">Lyme Disease Management,<br/>Made <span>Easy</span>.</h1>
+        <div className='row valign-wrapper'>
+          <div className='col s4 offset-s1 center-align'>
+            <div className='row'>
+              {/* <p className='landing_caption'>LymeLog is your personal tool for simplifying your treatment process while faciliating recovery, all in a convenient and user-friendly interface.</p> */}
+              <p className='landing__caption'>Organize your treatment process and track your progress with the click of a button!</p>
+            </div>
+            <div className='row'>
+              <Link className="landing__button left" to="/Signup">Get Started</Link>
+              <span>Or</span>
+              <Link className="landing__button right" to="/Login">Login</Link>
+            </div>
+          </div>
+          <div className='col s6'>
+            <video className='graph-screen-shot' src='/videos/Kazam_screencast_00000.mp4' poster='/images/LandingPageScreenshot.png' autoPlay loop></video>
+          </div>
+        </div>
 
+        {/* <Link className="down_icon" to="#2"><i className="large material-icons">keyboard_arrow_down</i></Link> */}
+        <a href='#2' className="down_icon"><i className="large material-icons">keyboard_arrow_down</i></a>
+      </div>
     </div>
-
-    {/* <div id="2" className="landing__section2">
-      <div className="flex-wrapper">
-        <h1>[Features]</h1>
+    <ScrollableAnchor id={'2'}>
+      <div className="landing__section2">
+        <div className="landing__section__flex-wrapper">
+          <h1>[Features]</h1>
+        </div>
       </div>
-    </div> */}
+    </ScrollableAnchor>
+
 
   </div>
 );
